6d526c997305002304a5164df6d4293b82869cb5
Ignatius Nothnagel <inothnagel@gmail.com>
2013-03-21 23:16:46 +0200

    daily email styling

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ActiveRecord::Base
   belongs_to :list
   attr_accessible :name, :note
+
+  def name_and_text_string
+    out = self.name
+    out += " - #{self.note}" if self.note
+    out
+  end
 end
