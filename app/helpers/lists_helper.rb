c2084f86f99713875a52dadfb2d9663608e4f8b3
Ignatius Nothnagel <inothnagel@gmail.com>
2013-03-29 16:58:02 +0200

    random item link

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,10 @@
 module ListsHelper
+  def random_item_link (list)
+    item = list.random_item
+    return "" if item.nil?
+
+    text = item.name
+    link = "http://inothnagel-random-reminder.herokuapp.com/items/#{item.id}"
+    link_to(text, link)
+  end
 end
