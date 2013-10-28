d3305143483539373a36e894a2968a65ed6e3e2b
Ignatius Nothnagel <inothnagel@gmail.com>
2013-10-28 16:06:10 +0200

    use jquery autocomplete on new item submit page

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -2,18 +2,29 @@ class SubmitController < ApplicationController
 
   def submit
     @item = Item.new
+    @lists = current_user.lists.map { |l| l.name }
     @name = params[:title]
     @url = params[:url]
   end
 
   def create
-    @list = List.find(params[:list][:list_id])
-    params[:item].delete(:list_id)
+    @list = current_user.lists.find_by_name(params[:item][:list])
+    @list = create_new_user_list(params[:item][:list]) unless @list
+
+    params[:item].delete(:list)
 
     @item = Item.new(params[:item])
     @item.list = @list
     @item.save
 
     redirect_to @list
+  end
+
+  def create_new_user_list(list_name)
+    list = List.new
+    list.name = list_name
+    list.user = current_user
+    list.save
+    list
   end
 end
